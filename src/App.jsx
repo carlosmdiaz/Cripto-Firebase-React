4fd1a0191a4e4049578bed4e918acb1da777da35
carlosmdiaz <carlosdiaz3979@gmail.com>
2024-03-17 15:08:45 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { ThemeProvider } from "./context/ThemeContext";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import Signin from "./routes/Signin";
+import Signup from "./routes/Signup";
+import Account from "./routes/Account";
+import axios from "axios";
+
+function App() {
+  const [coin, setCoin] = useState([]);
+
+  useEffect(() => {
+    const request = async () => {};
+  }, []);
+  return (
+    <ThemeProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/routes/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CoinSearch from "../components/CoinSearch";
+
+function Home() {
+  return (
+    <div>
+      <CoinSearch />
+    </div>
+  );
+}
+
+export default Home;
