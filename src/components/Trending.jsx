cdc6a8171e23ac8e1c7353214c95d36a92d0277f
carlosmdiaz <carlosdiaz3979@gmail.com>
2024-03-17 21:02:49 -0400

    In this commit I finished the Search Crypto Table. Also, I finished the trending component. This component shows the trending cryptos.

---
+++ src/components/Trending.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function Trending() {
+  const url = "https://api.coingecko.com/api/v3/search/trending";
+  const [trending, setTrending] = useState([]);
+  useEffect(() => {
+    const request = async () => {
+      await axios.get(url).then((response) => {
+        setTrending(response.data.coins);
+        console.log(response.data);
+      });
+    };
+    request();
+  }, []);
+  return (
+    <div className="rounded-div my-12 py-8 text-primary">
+      <h1 className="text-2xl font-bold py-4">Trending Coins</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {trending.map((coin) => {
+          return (
+            <div
+              key={coin.item.id}
+              className="rounded-div flex justify-between p-4 hover:scale-105 ease-in-out duration-300"
+            >
+              <div className="flex items-center justify-between w-full">
+                <div className="flex">
+                  <img
+                    className="mr-4 rounded-full"
+                    src={coin.item.small}
+                    alt={coin.item.id}
+                  />
+                  <div>
+                    <p className="font-bold">{coin.item.name}</p>
+                    <p>{coin.item.symbol}</p>
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <img
+                    src="https://assets.coingecko.com/coins/images/1/standard/bitcoin.png?1696501400"
+                    alt="Bitcoin Logo"
+                    className="w-4 mr-2"
+                  />
+                  <p>{coin.item.price_btc.toFixed(10)}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Trending;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CoinSearch from "../components/CoinSearch";
+import Trending from "../components/Trending.jsx";
 
 function Home(props) {
   const { coins } = props;
   return (
     <div>
       <CoinSearch coins={coins} />
+      <Trending />
     </div>
   );
 }
